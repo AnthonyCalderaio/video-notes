096d904e8d30e617c7a947084d36e0a7fe02862c
Anthony <anthonycalderaio@gmail.com>
2023-09-27 19:01:25 -0400

    loading works correct

--- src/app/services/loading-notification/loading-notification.service.ts
+++ src/app/services/loading-notification/loading-notification.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { ElementRef, Injectable, ViewChild } from '@angular/core';
+import { BehaviorSubject, interval } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingNotificationService {
+ loadingIndicatorRef = document.getElementById('loadingIndicator')
+
+
   private loadingSubject = new BehaviorSubject<boolean>(false);
   loading$ = this.loadingSubject.asObservable();
 
@@ -12,10 +15,17 @@ export class LoadingNotificationService {
 
   show(){
     this.loadingSubject.next(true);
+    interval(1000).subscribe(res => {
+      console.log(this.loadingIndicatorRef )
+    })
+    
+    this.loadingIndicatorRef?.setAttribute("style","display:flex");
+
   }
 
   hide(){
     this.loadingSubject.next(false);
+    this.loadingIndicatorRef?.setAttribute("style","display:none");
   }
 
 }
