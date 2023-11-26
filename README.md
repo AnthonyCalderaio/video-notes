355bc8760d0e2487608b488d826af216b5722bfb
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2023-11-26 02:08:38 -0500

    done

--- README.md
+++ README.md
@@ -14,6 +14,12 @@ Run `ng generate component component-name` to generate a new component. You can 
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
 
+## Build and Run Electron packaging
+
+$npx electron-packager .
+
+(https://github.com/electron/packager)
+
 ## Running unit tests
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
--- app.js
+++ app.js
@@ -1,50 +1,49 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 
-    const url = require("url");
-    const path = require("path");
+const url = require("url");
+const path = require("path");
 
-    let mainWindow
+let mainWindow
 
-    function createWindow () {
-      mainWindow = new BrowserWindow({
-        width: 1000,
-        height: 1000,
-        minWidth: 900,
-        minHeight: 900,
-        webPreferences: {
-          nodeIntegration: true,
-          enableRemoteModule: true,
-          // This file is where we run 'node' commands
-          preload: path.join(__dirname, "preload.js") // add "preload"
-        }
-      })
-      // mainWindow.setResizable(false)
-
-      mainWindow.loadURL(
-        url.format({
-          pathname: path.join(__dirname, `/dist/video-notes/index.html`),
-          protocol: "file:",
-          slashes: true
-        })
-      );
-      // Open the DevTools.
-      // mainWindow.webContents.openDevTools()
-
-      mainWindow.on('closed', function () {
-        mainWindow = null
-      })
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 1000,
+    minWidth: 900,
+    minHeight: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      // enableRemoteModule: true,
+      // This file is where we run 'node' commands
+      // preload: path.join(__dirname, "preload.js") // add "preload"
     }
+  })
 
-    // mainWindow.s
-    
-
-    app.on('ready', createWindow)
-
-    app.on('window-all-closed', function () {
-      if (process.platform !== 'darwin') app.quit()
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/video-notes/index.html`),
+      protocol: "file:",
+      slashes: true
     })
+  );
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 
-    app.on('activate', function () {
-      if (mainWindow === null) createWindow()
-      mainWindow.loadURL('http://localhost:4200');
-    })
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+
+app.on('ready', function () {
+  createWindow()
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+  // mainWindow.loadURL('http://localhost:4200');
+})
