77e9efb8b530f973fa051cf1068f24f14e19ed8b
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-10-14 12:35:22 -0400

    mock added

---
+++ src/mocks/savedPaths/saved-paths.mock.ts
@@ -0,0 +1,27 @@
+export const SavedPathsMock = [
+    {
+        "path": "/Users/anthony_calderaio/Desktop/Screen Recording 2024-08-07 at 11.17.54 AM.mov",
+        "notes": [
+            {
+                "timeSignature": "0",
+                "notes": "something!"
+            },
+            {
+                "timeSignature": "1",
+                "notes": "another thing"
+            },
+            {
+                "timeSignature": "2",
+                "notes": "ajhsjkhdasd"
+            },
+            {
+                "timeSignature": "3",
+                "notes": "whats the del with this part?"
+            }
+        ]
+    },
+    {
+        "path": "/Users/anthony_calderaio/Desktop/IMG_8888.MOV",
+        "notes": []
+    }
+]
